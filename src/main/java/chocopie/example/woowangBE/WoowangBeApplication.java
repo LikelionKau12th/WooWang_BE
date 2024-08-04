cae7e17d0baa989b44ae1a129b93f3afb7f4a5d0
jjeongee <starstar117@naver.com>
2024-08-04 23:58:05 +0900

    DB기본설정,memberservice고민중

---
+++ src/main/java/chocopie/example/woowangBE/WoowangBeApplication.java
@@ -0,0 +1,13 @@
+package chocopie.example.woowangBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowangBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoowangBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chocopie/example/woowangBE/WoowangBeApplicationTests.java
@@ -0,0 +1,13 @@
+package chocopie.example.woowangBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowangBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
